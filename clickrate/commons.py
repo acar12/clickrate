3288bdd51d574604fef4b03852f519cfd883f1db
acar12 <39702495+acar12@users.noreply.github.com>
2022-07-22 13:55:41 -0500

    proper, timeless tests

--- clickrate/commons.py
+++ clickrate/commons.py
@@ -14,15 +14,27 @@ def clean_text(text: str) -> list[str]:
 
 def get_headlines():
     # buzzfeed
-    content = requests.get("https://www.buzzfeed.com/").content
+    content = requests.get("https://web.archive.org/web/20220721140241/https://www.buzzfeed.com/").content
     soup = Soup(content, "html.parser")
     for e in soup.find_all(class_="feedItem_title__0_9qB"):
         yield e.contents[0], 1
     # associated press
-    content = requests.get("https://apnews.com/").content
+    content = requests.get("https://web.archive.org/web/20220722180934/https://apnews.com/hub/ap-top-news").content
     soup = Soup(content, "html.parser")
-    for e in soup.find_all(class_="headline-0-2-153"):
+    for e in soup.find_all("h2"):
         yield e.contents[0], 0
 
 def get_path(file_name: str) -> str:
     return str(os.path.join(os.path.dirname(__file__), file_name))
+
+def print_confusion_matrix(matrix: list[int]) -> float:
+    true_pos, true_neg, false_pos, false_neg = matrix
+    false_pos_rate = false_pos / (true_pos + false_pos) * 100
+    false_neg_rate = false_neg / (true_neg + false_neg) * 100
+    accuracy = (true_pos + true_neg) / (true_pos + true_neg + false_pos + false_neg) * 100
+
+    print(f"\nFalse positive rate: {false_pos_rate:.1f}%")
+    print(f"False negative rate: {false_neg_rate:.1f}%")
+    print(f"Accuracy: {accuracy:.1f}%")
+
+    return accuracy
--- clickrate/test_model.py
+++ clickrate/test_model.py
@@ -1,41 +1,48 @@
 """
 Test the model to see if it actually works.
 """
+import os, sys
+sys.path.append(os.path.dirname(__file__))
 
-from clickrate.model import ClickbaitClassifier
 import pandas as pd
 import unittest
 from typing import Iterable
-from clickrate.commons import get_headlines, get_path
+from model import ClickbaitClassifier
+from commons import get_headlines, get_path, print_confusion_matrix
 
 class TestModel(unittest.TestCase):
     def setUp(self):
         self.model = ClickbaitClassifier()
 
-    def get_accuracy(self, iterable: Iterable[tuple[str, int]]) -> float:
-        avg_accuracy = 0
-        num_rows = 0
+    def get_accuracy(self, iterable: Iterable[tuple[str, int]]) -> list[int]:
+        confusion_matrix = [0, 0, 0, 0] # true positive, true negative, false positive, false negative
 
         for word, val in iterable:
             guess = round(self.model.predict_clickbait(word))
             if guess == val:
-                avg_accuracy += 1
-            num_rows += 1
+                if val == 0:
+                    confusion_matrix[1] += 1
+                elif val == 1:
+                    confusion_matrix[0] += 1
+            else:
+                if val == 0:
+                    confusion_matrix[2] += 1
+                elif val == 1:
+                    confusion_matrix[3] += 1
         
-        avg_accuracy /= num_rows
-        return avg_accuracy
+        return confusion_matrix
 
     def test_model_csv(self):
         df = pd.read_csv(get_path("data/test_headlines.csv"))
-        accuracy = self.get_accuracy((row for _, row in df.iterrows()))
-        self.assertGreaterEqual(accuracy, 0.8)
-        print(f"{int(accuracy * 100)}%")
+        confusion_matrix = self.get_accuracy((row for _, row in df.iterrows()))
+        accuracy = print_confusion_matrix(confusion_matrix)
+        self.assertGreaterEqual(accuracy, 85)
 
     def test_model_list(self):
         # a few headlines gathered from buzzfeed and reuters to test modern media
-        accuracy = self.get_accuracy(get_headlines())
-        self.assertGreaterEqual(accuracy, 0.8)
-        print(f"{int(accuracy * 100)}%")
+        confusion_matrix = self.get_accuracy(get_headlines())
+        accuracy = print_confusion_matrix(confusion_matrix)
+        self.assertGreaterEqual(accuracy, 85)
 
 if __name__ == "__main__":
     unittest.main()
